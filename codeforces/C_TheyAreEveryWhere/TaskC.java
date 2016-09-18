d9a1c1fed62f8a780dcc5b241549a7bece02c2ab
thanhld94 <thanhld94@gmail.com>
2016-09-18 12:28:19 -0500

    Codeforces round #364 C

--- codeforces/C_TheyAreEveryWhere/TaskC.java
+++ codeforces/C_TheyAreEveryWhere/TaskC.java
@@ -1,9 +1,7 @@
 import java.io.PrintWriter;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class TaskC{
+public class TaskC {
   public static void main(String[] args) {
     TaskC tC = new TaskC();
     PrintWriter pw = new PrintWriter(System.out);
@@ -12,69 +10,44 @@ public class TaskC{
   }
 
   public void solve(Scanner input, PrintWriter output) {
-  	int n = input.nextInt();
-  	List<List<Integer>> app = new ArrayList<List<Integer>>();
-  	for (int i = 0; i < 52; i++) {
-  		app.add(new ArrayList<Integer>());
-  	}
+    int n = input.nextInt();
+    String text = input.next();
+    Set <Character> pokes = new HashSet <Character>();
+    
+    for (int i = 0; i < n; i++) {
+      char type = text.charAt(i);
+      pokes.add(type);
+    }
 
-  	String flat = input.next();
-  	for (int i = 0; i < n; i++) {
-  		int hash = hashcode(flat.charAt(i));
-  		app.get(hash).add(i);
-  	}
+    Map <Character, Integer> last = new HashMap <Character, Integer>();
+    for (char poke : pokes) {
+      last.put(poke, -1);
+    }
 
-  	int l = 1;
-  	int r = n;
-  	int result = 1;
-  	while (l <= r) {
-  		int mid = (l + r) / 2;
-  		if (cancover(flat, mid, app)) {
-  			result = mid;
-  			r = mid - 1;
-  		} else {
-  			l = mid + 1;
-  		}
-  	}
-  	output.println(result);
-  }
+    int result = n;
+    for (int i = 0; i < n; i++) {
+      char type = text.charAt(i);
+      last.put(type, i);
 
-  private boolean cancover(String flat, int range, List<List<Integer>> appearance) {
-  	for (int i = 0; i <= flat.length() - range; i++) {
-  		boolean found = true;
-	  	for (int j = 0; j < 52; j++) {
-	  		if (appearance.get(j).size() > 0) {
-	  			int l = 0;
-	  			int r = appearance.get(j).size() - 1;
-	  			int res = 0;
-	  			while (l <= r) {
-	  				int mid = (l + r) / 2;
-	  				if (appearance.get(j).get(mid) >= i) {
-	  					res = mid;
-	  					r = mid - 1;
-	  				} else {
-	  					l = mid + 1;
-	  				}
-	  			}
+      boolean valid = true;
+      for (char poke : pokes) {
+        if (last.get(poke) == -1) {
+          valid = false;
+          break;
+        }
+      }
 
-	  			if (i > appearance.get(j).get(res) || appearance.get(j).get(res) > i + range - 1) {
-	  				found = false;
-	  				break;
-	  			}
-	  		}
-  		}
-  		if (found) {
-  			return true;
-  		}
-  	}
-  	return false;
-  }
-
-  private int hashcode(char c) {
-  	if ('a' <= c && c <= 'z') {
-  		return c - 'a';
-  	} else {
-  		return 26 + c - 'A';
-  	}
+      if (valid) {
+        int first = Integer.MAX_VALUE;
+        //output.println("i = " + i);
+        for (char poke : pokes) {
+          first = Math.min(first, last.get(poke));
+          //output.println(" last " + poke + " at " + last.get(poke));
+        }
+        //output.println(" -> len = " + (i - first + 1));
+        result = Math.min(result, i - first + 1);
+      }
+    }
+    output.println(result);
   }
 }
