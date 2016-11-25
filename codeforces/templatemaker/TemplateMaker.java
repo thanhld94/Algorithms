9d6ca04a7cbcefca9d6cd6085df068fbced9699b
thanhld94 <thanhld94@gmail.com>
2016-11-25 12:35:06 -0600

    Implemented custom bufferedReader in java template

--- codeforces/templatemaker/TemplateMaker.java
+++ codeforces/templatemaker/TemplateMaker.java
@@ -23,16 +23,55 @@ public class TemplateMaker {
     output.println("public class Task" + task + " {");
     
     //main method
-    output.println("  public static void main(String[] args) throws IOException {");
+    output.println("  public static void main(String[] args) {");
     output.println("    Task" + task + " t" + task + " = new Task" + task + "();");
-    output.println("    PrintWriter out = new PrintWriter(System.out);");
-    output.println("    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));");
+    output.println("    BScanner in = new BScanner();");
+    output.println("    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));");
     output.println("    t" + task + ".solve(in, out);");
     output.println("    out.close();");
     output.println("  }\n");
     
     //solve method
-    output.println("  public void solve(BufferedReader in, PrintWriter out) throws IOException {");
+    output.println("  public void solve(BScanner in, PrintWriter out) {");
+    output.println("  }\n");
+
+    //Scanner class
+    output.println("  private static class BScanner {");
+    output.println("    private BufferedReader input;");
+    output.println("    private StringTokenizer st;\n");
+    output.println("    private BScanner() {");
+    output.println("      input = new BufferedReader(new InputStreamReader(System.in));");
+    output.println("    }\n");
+    output.println("    private String next() {");
+    output.println("      while (st == null || !st.hasMoreElements()) {");
+    output.println("        try {");
+    output.println("          st = new StringTokenizer(input.readLine());");
+    output.println("        } catch (IOException e) {");
+    output.println("          System.err.println(\"No more token\");");
+    output.println("          e.printStackTrace();");
+    output.println("        }");
+    output.println("      }");
+    output.println("      return st.nextToken();");
+    output.println("    }\n");
+    output.println("    private int nextInt() {");
+    output.println("      return Integer.parseInt(next());");
+    output.println("    }\n");
+    output.println("    private long nextLong() {");
+    output.println("      return Long.parseLong(next());");
+    output.println("    }\n");
+    output.println("    private double nextDouble() {");
+    output.println("      return Double.parseDouble(next());");
+    output.println("    }\n");
+    output.println("    private String nextLine() {");
+    output.println("      String line = \"\";");
+    output.println("      try {");
+    output.println("        line = input.readLine();");
+    output.println("      } catch (Exception e) {");
+    output.println("        System.err.println(\"No more lines\");");
+    output.println("        e.printStackTrace();");
+    output.println("      }");
+    output.println("      return line;");
+    output.println("    }");
     output.println("  }");
 
     //end class
